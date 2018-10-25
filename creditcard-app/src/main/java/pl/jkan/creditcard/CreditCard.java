f7f26781fa3c9c876faa281c8485da8f3f0f1ffe
= <=>
2018-10-25 10:01:58 +0000

    card blocking implementation

--- creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -1,9 +1,17 @@
 package pl.jkan.creditcard;
 
 class CreditCard {
+    private boolean blocked = false;
     public void assignLimit(double limit) {
         
     }   
+    
+    public boolean isBlocked() {
+        return this.blocked;
+    }
+    public void block() {
+        this.blocked = true;
+    }
     
     public double getLimit() {
         return 2000;
