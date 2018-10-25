49d7db73edbebd98e7c72f727da103db722f21f1
= <=>
2018-10-25 10:27:40 +0000

    withdraw implementation

--- creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -1,11 +1,22 @@
 package pl.jkan.creditcard;
 
 class CreditCard {
+    private double balance = 0;
+    private double cardlimit = 0;
     public void assignLimit(double limit) {
-        
+        balance = limit;
+        cardlimit = limit;
     }   
     
     public double getLimit() {
         return 2000;
+    }
+    
+    public void withdraw(double money) {
+        balance = balance - money;
+    }
+    
+    public double getCountBalance() {
+        return balance;
     }
 }
